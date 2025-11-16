e0e1442b80ab3df1bbb91725803d27d7bce858f4
coolight <2465045051@qq.com>
2025-11-16 13:45:19 +0800

    fix zlib install

--- buildscripts/scripts/zlib.sh
+++ buildscripts/scripts/zlib.sh
@@ -37,7 +37,6 @@ CONF=1 "${MY_CMAKE_EXE_DIR}/cmake" -S.. -B. \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_LIBDIR=lib \
     -DCMAKE_FIND_ROOT_PATH=${prefix_dir} \
-    -DINSTALL_PKGCONFIG_DIR=${prefix_dir}/lib/pkgconfig \
     -DBUILD_SHARED_LIBS=OFF \
     -DSKIP_INSTALL_LIBRARIES=OFF \
     -DZLIB_COMPAT=ON \
